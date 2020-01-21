d8a1d1ec7626b22177279e38c3e69b7b694a785c
Grado superior DAM <Grado superior DAM@DESKTOP-EDAD1PC>
2020-01-21 20:23:13 +0100

    hola buenas tardes

---
+++ src/Prueba01/Borjeles01.java
@@ -0,0 +1,7 @@
+package Prueba01;
+public class Borjeles01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
---
+++ src/Prueba01/Comparacion1.java
@@ -0,0 +1,10 @@
+package Prueba01;
+
+public class Comparacion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
